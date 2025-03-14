f3c57a274d81195d0622854766f42c173a611af0
GuySchnidrig <g.schnidu@gmail.com>
2025-03-14 11:22:50 +0100

    update

--- content/pages/about.md
+++ content/pages/about.md
@@ -2,4 +2,4 @@ Title: About Me
 Date: 2025-03-13
 Slug: about
 
-Hello! I’m Guy Schndrig, a passionate data scientist and content creator.
+Hello! I’m Guy Schndrig, a data scientist working at the FSVO.
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,33 +1,77 @@
+from datetime import datetime
+
+# Author and Title
 AUTHOR = 'Guy Schnidrig'
 SITENAME = 'Guy Schnidrig'
 SITEURL = ""
 
+SITETITLE = "Guy Schnidrig, PhD"
+SITESUBTITLE = "Data Scientist"
+SITEDESCRIPTION = "Personal Website of Guy Schnidrig"
+
+
+# Use the Flex Theme
+THEME = "themes/Flex"
+BROWSER_COLOR = "#333"  # Change the browser tab color on mobile
+PYGMENTS_STYLE = "monokai"
+ROBOTS = "index, follow"
+
+# Paths
 PATH = "content"
 OUTPUT_PATH = 'docs'
 RELATIVE_URLS = True
+DISABLE_URL_HASH = True
+DEFAULT_PAGINATION = 5
 
+# Time and Language
 TIMEZONE = 'Europe/Rome'
-
-DEFAULT_LANG = 'en'
+I18N_TEMPLATES_LANG = "en"
+DEFAULT_LANG = "en"
 
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+
 # Blogroll
+SOCIAL = (("github", "https://github.com/GuySchnidrig"),
+          ("linkedin", "https://www.linkedin.com/in/guy-schnidrig/"),
+          ("rss", "/blog/feeds/all.atom.xml"),
+          )
 
-# Enable Social Links (Optional)
-SOCIAL = (("github", "https://github.com/GuySchnidrig"),)
 
-DEFAULT_PAGINATION = 5
+GITHUB_CORNER_URL = "https://github.com/GuySchnidrig"
+
+# Menu
+MENUITEMS = (
+    ("About Me", "/pages/about.html"),
+    ("Contact", "/pages/contact.html"),
+)
+
+# License
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike 4.0 International License",
+    "version": "4.0",
+    "slug": "by-sa",
+    "icon": True,
+    "language": "en_US",
+}
+
+COPYRIGHT_YEAR = datetime.now().year
 
 
-# Use the Flex Theme
-THEME = "themes/Flex"
-FAVICON = "images/favicon.ico"
-BROWSER_COLOR = "#333"  # Change the browser tab color on mobile
+DISQUS_SITENAME = "Guy-Schnidrig"
+ADD_THIS_ID = "42"
 
+# Darkmode
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+USE_LESS = True
