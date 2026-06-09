d1e9f8558042c4309a351d10e8eb9fc8a00cdf61
Guy Schnidrig BLV <78217667+GuySchnidrig@users.noreply.github.com>
2026-06-09 09:38:22 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -1,23 +1,14 @@
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
-
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-# If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://guyschnidrig.github.io/"
+SITEURL = "https://guyschnidrig.github.io"
+SITELOGO = SITEURL + "/images/favicon.svg"
 RELATIVE_URLS = False
-STATIC_PATHS = ['images']
-
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
-
+OUTPUT_PATH = "output"          # override the 'docs' from pelicanconf.py
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-# DISQUS_SITENAME = ""
-# GOOGLE_ANALYTICS = ""
+STATIC_PATHS = ['images']
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
